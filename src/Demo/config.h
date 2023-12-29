9d7b68ffc1080faccbf25bd06e7207833729859c
heinrichAdams <hein.peter.adams@gmail.com>
2023-12-29 12:39:52 +0000

    Config File For Debug Demo

---
+++ src/Demo/config.h
@@ -0,0 +1,18 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// WINDOW PROPERTIES
+// ---------------------------------------------
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
+
+#define WINDOW_TITLE "Demo"
+// ---------------------------------------------
+
+// INPUT OPTIONS
+// ---------------------------------------------
+#define KEYBOARD_SIZE 350
+// ---------------------------------------------
+
+
+#endif
